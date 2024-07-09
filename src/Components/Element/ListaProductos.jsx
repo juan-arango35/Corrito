cafebbb05991eccdaf78bb6c22b3053621817065
juan-arango35 <128092743+juan-arango35@users.noreply.github.com>
2024-07-09 16:01:56 -0500

    agrega styled components a lista prodcutos

--- src/Components/Element/ListaProductos.jsx
+++ src/Components/Element/ListaProductos.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import TarjetaProducto from './TarjetaProducto'
+import TarjetaProducto from './TarjetaProducto';
+import styled from 'styled-components';
 
 const ListaProductos = ({productos}) => {
   return (
-    <div>
+    <Container>
         <h1>Lista de productos</h1>
-        <hr />
         <TarjetaProducto productos={productos}/>
-    </div>
+    </Container>
   )
 }
 
 export default ListaProductos
+
+const Container=styled.div`
+background: gray;
+width: 70%;
+`
