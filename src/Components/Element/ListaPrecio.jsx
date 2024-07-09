89a022f871aff30f20d17610a4a5f3b1375443de
juan-arango35 <128092743+juan-arango35@users.noreply.github.com>
2024-07-09 16:03:02 -0500

    agrega styled component a lista precio

---
+++ src/Components/Element/ListaPrecio.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const ListaPrecio = () => {
+  return(  <Container>
+  <h1>Lista precio</h1>
+  <div>
+    <p>Producto</p>
+    <p>precio</p>
+    <p>cantidad</p>
+    <p>total</p>
+    <button>Agregar Al carrito</button>
+
+    
+  </div>
+  </Container>);
+};
+
+export default ListaPrecio;
+
+const Container= styled.div`
+background:skyblue;
+width: 30%;
+`
+
+
